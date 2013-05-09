25168adaef9077e25ea69444f62219e853827ddc
Neo <Neo@192.168.43.104>
2013-05-09 12:36:18 +0300

    Added a new method "mysql_prep" which acts as a good way to prepare data for MySql

--- includes/database.php
+++ includes/database.php
@@ -22,6 +22,30 @@
                 unset($this->connection);
             }
         }
+        public function query($sql){
+            $result - mysql_query($sql, $this->connection);
+            $this->confirm_query($result);
+            return $result;
+        }
+        public function confirm_query($result){
+            if(!$result){
+                die("Database query failed: ".mysql_error());
+            }
+        }
+        public function mysql_prep($value){
+            $magic_quotes_active = get_magic_quotes_gpc();
+            $new_enough_php = function_exists("mysql_real_espace_sting"); // i.e. PHP >= v4.3.0
+            if($new_enough_php){// PHP v4.3.0 higher
+                //undo any magic quote effects s mysql_real_escape_string can do the work
+                if($magic_quotes_active){$value = stripslashes($value);}
+                $value = mysql_real_escape_string($value);
+            }else{ // before PHP v4.3.0
+                //if magic quotes aren't already on then add slashes manually
+                if (!$magic_quotes_active){ $value = addslashes($value);}
+                //if magic quotes are active, then the slashes already exist
+            }
+            return $value;
+        }
     }
     
     $database = new MySQLDatabase();
