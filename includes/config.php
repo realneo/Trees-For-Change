1fba13ee733820839900f5eba9abd20a66acad27
Neo <Neo@192.168.43.104>
2013-05-09 11:35:16 +0300

    Added the Config.php file which has database constants

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+    // Databse Constants
+    define("DB_SERVER", "localhost:8888");
+    define("DB_USER", "neo");
+    define("DB_PASS", "pHpMaster7");
+    define("DB_NAME", "tfc_db");
+?>
