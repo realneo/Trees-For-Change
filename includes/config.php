b9fc23486a008f2622c1850278cfd45587d846b9
Nadhir Mubarak Bahayan <neo@yoteyote.com>
2013-05-09 11:47:25 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,6 @@
     // Databse Constants
     defined('DB_SEVER') ? null : define("DB_SERVER", "localhost:8888");
     defined('DB_USER') ? null : define("DB_USER", "neo");
-    defined('DB_PASS') ? null : define("DB_PASS", "jQueryMaster7");
+    defined('DB_PASS') ? null : define("DB_PASS", "password");
     defined('DB_NAME') ? null : define("DB_NAME", "tfc_db");
 ?>
