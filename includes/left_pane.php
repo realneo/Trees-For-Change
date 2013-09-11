7a2fca94900df7a08c4b8e97df92d93a6438c05d
Neo <Neo@192.168.43.104>
2013-09-11 09:30:10 +0300

    adds and removes links from the `left_pane`

--- includes/left_pane.php
+++ includes/left_pane.php
@@ -2,8 +2,10 @@
     <ul class="nav nav-list green_hover">
         <li class="active"><a href="#">Dashboard</a></li>
         <li class="nav-header">SELL TREES</li>
-        <li><a href="#">Sell a Tree</a></li>
-        <li><a href="#">Sell Trees</a></li>
+        <li><a href="sell.php">Sell Trees</a></li>
+        <li class="nav-header">ACTION</li>
+        <li><a href="assigning.php">Pending Assigning</a></li>
+        <li><a href="planting.php">Pending Planting</a></li>
         <li class="nav-header">ADD DATA</li>
         <li><a href="add_stock.php">Add Stock</a></li>
         <li><a href="add_tree_type.php">Add Tree Type</a></li>
@@ -15,7 +17,6 @@
         <li><a href="view_nursery.php">Nurseries</a></li>
         <li><a href="view_supplier.php">Suppliers</a></li>
         <li><a href="view_donor.php">Donors</a></li>
-        <li><a href="view_plaque_names">Plaque Names</a></li>
         <li class="nav-header">REPORTS</li>
         <li><a href="#">Overall Report</a></li>
         <li><a href="#">Pending Report</a></li>
