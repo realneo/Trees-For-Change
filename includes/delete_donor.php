b44ba127ce10a9d45f55974ccc8a94059514214b
Nadhir Bahayan <nbahayan@gmail.com>
2013-09-21 23:47:55 +0300

    Add / Remove Donor

--- includes/delete_donor.php
+++ includes/delete_donor.php
@@ -5,13 +5,15 @@
     // Getting Data from the Form
     $id = $_GET['id'];
     
-    $q = "DELETE FROM `donor` WHERE `id` = '$id'";
+    // Cheking to see if donor is associated with any transaction
+    $qq = mysql_query("SELECT * FROM `sold` WHERE `donor_id` = '$id'");
     
-    if(mysql_query($q) == true){
-        $_SESSION['alert'] = "<div class='alert alert-success'>Donor was successfully Deleted</div>";
+    if(mysql_num_rows($qq) >= 1){
+        $_SESSION['alert'] = "<div class='alert alert-danger'>This Donor is associated with some record in the Database. This Item cannot be deleted</div>";
         header("Location: ../add_donor.php");
     }else{
-        $_SESSION['alert'] = "<div class='alert alert-error'>There was an error deleting this Donor</div>";
+        mysql_query("DELETE FROM `donor` WHERE `id` = '$id'");
+        $_SESSION['alert'] = "<div class='alert alert-success'>Donor was successfully Deleted</div>";
         header("Location: ../add_donor.php");
     }
 
