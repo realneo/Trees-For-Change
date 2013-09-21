91cf19c24b2568a0a2c7b4a2eb1e53c14f92c5b9
Nadhir Bahayan <nbahayan@gmail.com>
2013-09-21 23:10:00 +0300

    Add / Remove Location is Fixed

--- includes/delete_location.php
+++ includes/delete_location.php
@@ -4,14 +4,17 @@
     
     // Getting Data from the Form
     $id = $_GET['id'];
+        
+    // Cheking to see if location is associated with any transaction
+    $qq = mysql_query("SELECT * FROM `sold` WHERE `location_id` = '$id'");
+    $qqq = mysql_query("SELECT * FROM `nursery` WHERE `location_id` = '$id'");
     
-    $q = "DELETE FROM `location` WHERE `id` = '$id'";
-    
-    if(mysql_query($q) == true){
-        $_SESSION['alert'] = "<div class='alert alert-success'>Location was successfully Deleted</div>";
+    if(mysql_num_rows($qq) >= 1 || mysql_num_rows($qqq) >= 1){
+        $_SESSION['alert'] = "<div class='alert alert-error'>This Location is associated with some record in the Database. This Item cannot be deleted</div>";
         header("Location: ../add_location.php");
     }else{
-        $_SESSION['alert'] = "<div class='alert alert-error'>There was an error deleting this Location</div>";
+        mysql_query("DELETE FROM `location` WHERE `id` = '$id'");
+        $_SESSION['alert'] = "<div class='alert alert-success'>Location was successfully Deleted</div>";
         header("Location: ../add_location.php");
     }
 
